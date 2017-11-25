22b3b871d593359156f0ad9f347d206b181627d2
ttm <taras.tataryn.ttm@gmail.com>
2017-11-25 15:50:40 +0200

    Add JS logic to filter

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
+
+/* Index.html variables*/
 var searchBtn = document.querySelector(".hotel-search-btn");
 var searchBlock = document.querySelector(".modal-content");
 
 /* logic vars */
-
 var plusAdult = document.querySelector(".js-adult-plus");
 var minusAdult = document.querySelector(".js-adult-minus");
 var adultField = document.querySelector("#adult-number");
@@ -11,22 +12,32 @@ var plusChildren = document.querySelector(".js-children-plus");
 var minusChildren = document.querySelector(".js-children-minus");
 var childrenField = document.querySelector("#children-number");
 
-/* SHOW MODAl WINDOW */
-searchBtn.addEventListener("click", function(event){
-	event.preventDefault();
+/* hotels.html variables*/
+var filterRangeFrom = document.querySelector(".filter-range-from");
+var filterRangeTo = document.querySelector(".filter-range-to");
 
-	if (!searchBlock.classList.contains("modal-content-show"))
-		searchBlock.classList.add("modal-content-show");
-	else
-		searchBlock.classList.remove("modal-content-show");
-});
+/* SHOW MODAl WINDOW */
+if(searchBtn)
+	searchBtn.addEventListener("click", function(event){
+		event.preventDefault();
+
+		if (!searchBlock.classList.contains("modal-content-show"))
+			searchBlock.classList.add("modal-content-show");
+		else
+			searchBlock.classList.remove("modal-content-show");
+	});
+
 
 /* ADD AND SUBTRACT TOURISTS */
-plusAdult.addEventListener("click", addAdult);
-minusAdult.addEventListener("click", subtractAdult);
+if(plusAdult)
+	plusAdult.addEventListener("click", addAdult);
+if(minusAdult)
+	minusAdult.addEventListener("click", subtractAdult);
 
-plusChildren.addEventListener("click", addChild);
-minusChildren.addEventListener("click", subtractChild);
+if(plusChildren)
+	plusChildren.addEventListener("click", addChild);
+if(minusChildren)
+	minusChildren.addEventListener("click", subtractChild);
 
 function addAdult(){
 	adultField.value++;
@@ -50,18 +61,40 @@ function subtractChild(){
 		alert("CRITICAL ERROR 404");
 }
 
-adultField.addEventListener("keydown", function(event){
-	event.preventDefault();
-	if (event.keyCode === 40 && adultField.value > 0)
-		adultField.value--;
-	else if (event.keyCode === 38 && adultField.value >= 0)
-		adultField.value++;
-});
+if(adultField)
+	adultField.addEventListener("keydown", function(event){
+		event.preventDefault();
+		if (event.keyCode === 40 && adultField.value > 0)
+			adultField.value--;
+		else if (event.keyCode === 38 && adultField.value >= 0)
+			adultField.value++;
+	});
 
-childrenField.addEventListener("keydown", function(event){
-	event.preventDefault();
-	if (event.keyCode === 40 && childrenField.value > 0)
-		childrenField.value--;
-	else if (event.keyCode === 38 && childrenField.value >= 0)
-		childrenField.value++;
-})
+if(childrenField)
+	childrenField.addEventListener("keydown", function(event){
+		event.preventDefault();
+		if (event.keyCode === 40 && childrenField.value > 0)
+			childrenField.value--;
+		else if (event.keyCode === 38 && childrenField.value >= 0)
+			childrenField.value++;
+	});
+
+/* Filter logic */
+
+if(filterRangeFrom)
+	filterRangeFrom.addEventListener("keydown", function(event){
+		event.preventDefault();
+		if (event.keyCode === 40 && filterRangeFrom.value > 0)
+			filterRangeFrom.value--;
+		else if (event.keyCode === 38 && filterRangeFrom.value < filterRangeTo.value)
+			filterRangeFrom.value++;
+	});
+
+if(filterRangeTo)
+	filterRangeTo.addEventListener("keydown", function(event){
+		event.preventDefault();
+		if (event.keyCode === 40 && filterRangeTo.value > filterRangeFrom.value)
+			filterRangeTo.value--;
+		else if (event.keyCode === 38 && filterRangeTo.value < 3600)
+			filterRangeTo.value++;
+	});
